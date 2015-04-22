fb79876016634512f3532ec87c3eb355061df00d
chenyu <1916310286@qq.com>
2015-04-23 05:52:31 +0800

    no message

--- src/lager_file_backend.erl
+++ src/lager_file_backend.erl
@@ -134,10 +134,17 @@ handle_call(_Request, State) ->
 
 %% @private
 handle_event({log, Message},
-    #state{name=Name, level=L,formatter=Formatter,formatter_config=FormatConfig} = State) ->
+             #state{name=Name, level=L,formatter=Formatter,formatter_config=FormatConfig} = State) ->
     case lager_util:is_loggable(Message,L,{lager_file_backend, Name}) of
         true ->
-            {ok,write(State, lager_msg:timestamp(Message), lager_msg:severity_as_int(Message), Formatter:format(Message,FormatConfig)) };
+            %% A = lists:dropwhile(fun($\n)->true;
+            %%                        (_)->false
+            %%                     end,Formatter:format(Message,FormatConfig)),
+            %% io:format("_140:~ts",[A]),            
+            {ok,write(State, lager_msg:timestamp(Message), lager_msg:severity_as_int(Message), 
+                      %% lists:delete($\n,Formatter:format(Message,FormatConfig))) };
+                      re:replace(Formatter:format(Message,FormatConfig), "[\n]", "", [global, {return, list}])++ "\n")};
+            %% {ok,write(State, lager_msg:timestamp(Message), lager_msg:severity_as_int(Message), A) };
         false ->
             {ok, State}
     end;
